aff6d8c2d9bdebd98e5c9acdb0f2d2e836cf89b9
Irina Kurteva <igkurteva@gmail.com>
2022-02-10 22:41:46 +0200

    add testing and more layers

--- main.py
+++ main.py
@@ -13,10 +13,14 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.inputLayer = nn.Linear(sampleSize, self.hiddenSize).double()
+        self.middleLayer = nn.Linear(self.hiddenSize, self.hiddenSize).double()
+        self.middleLayer2 = nn.Linear(self.hiddenSize, self.hiddenSize).double()
         self.outputLayer = nn.Linear(self.hiddenSize, 1).double()
 
     def forward(self, x):
         x = torch.sigmoid(self.inputLayer(x))
+        x = torch.sigmoid(self.middleLayer(x))
+        x = torch.sigmoid(self.middleLayer2(x))
         x = self.outputLayer(x)
         x = torch.flatten(x)
         return x
@@ -32,29 +36,40 @@ class MyNetwork:
 
         self.transform = lambda star: (star.data, float(star.temp) / 10000)
         self.trainSet = StarDataset("/home/ikurteva/ai/stars/train", self.transform)
+        self.testSet = StarDataset("/home/ikurteva/ai/stars/test", self.transform)
 
         self.trainLoader = DataLoader(self.trainSet, batch_size=10, shuffle=False)
+        self.testLoader = DataLoader(self.testSet, batch_size=10, shuffle=False)
 
     def train(self):
         for epoch in range(100):
-            avgLoss = 0
-            avgC = 0
             for flux, temp in self.trainLoader:
                 self.optimizer.zero_grad()
                 output = self.net(flux)
 
                 loss = self.criterion(output, temp)
-                avgLoss += float(loss)
-                avgC += 1
                 # print(output, temp)
                 # print(loss)
 
                 loss.backward()
                 self.optimizer.step()
     
-            avgLoss /= avgC
-            print(avgLoss)
             self.scheduler.step(loss)
+            if epoch % 5 == 0:
+                self.test(self.trainLoader, "train")
+                self.test(self.testLoader, "test")
+
+    def test(self, data, typ):
+        lossfun = nn.MSELoss()
+        avgLoss = 0
+        avgC = 0
+        for flux, temp in data:
+            output = self.net(flux)
+            loss = lossfun(output, temp)
+            avgLoss += float(loss)
+            avgC += 1
+        avgLoss /= avgC
+        print('Avg {} Loss: {}'.format(typ, avgLoss))
 
 if __name__ == '__main__':
     MyNetwork().train()
